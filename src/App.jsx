902aa3c3a44f31cd54543d5bbc6dac96b22156e4
Pushkar Dangi <pushkardangi746@gmail.com>
2024-12-14 23:57:00 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -40,20 +40,11 @@ const router = createBrowserRouter(
       element: <PageNotFound />,
     },
   ],
-  {
-    future: {
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_skipActionErrorRevalidation: true,
-    },
-  }
 );
 
 function App() {
   return (
-    <RouterProvider future={{ v7_startTransition: true }} router={router} />
+    <RouterProvider router={router} />
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_NODE_ENV === "production"
+    baseURL: import.meta.env.MODE === "production"
         ? "https://myconnections-backend.onrender.com/auth"
         : "http://localhost:8080/auth",
     withCredentials: true,
